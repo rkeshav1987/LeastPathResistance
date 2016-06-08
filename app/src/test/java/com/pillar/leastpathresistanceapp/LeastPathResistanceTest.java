7036341dfe2ff850f9feecf01adf460b708458bc
rkeshav1987 <rkeshav1987>
2016-06-07 21:16:39 -0700

    passed test case for the given input data while calculating the least resistance path

--- app/src/test/java/com/pillar/leastpathresistanceapp/LeastPathResistanceTest.java
+++ app/src/test/java/com/pillar/leastpathresistanceapp/LeastPathResistanceTest.java
@@ -23,7 +23,6 @@ public class LeastPathResistanceTest {
         LeastPathResistance leastPathResistance = new LeastPathResistance();
         String data="3 4 1 2 8 6 6 1 8 2 7 4 5 9 3 9 9 5 8 4 1 3 2 6 3 7 2 8 6 4";
         String result = leastPathResistance.printleastResistancePath(data, 5, 6);
-        assertEquals("YES  16  1 2 3 4 4",result);
+        assertEquals("YES  16  1 2 3 4 4 5",result);
     }
-
 }
