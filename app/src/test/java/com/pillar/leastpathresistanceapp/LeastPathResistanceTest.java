7ff00dce6b3926e0cd3c0e8845f1b2044ba2e430
rkeshav1987 <rkeshav1987>
2016-06-07 21:45:37 -0700

    passed test case2 for the given input data to return result as yes while calulating the least resistance path

--- app/src/test/java/com/pillar/leastpathresistanceapp/LeastPathResistanceTest.java
+++ app/src/test/java/com/pillar/leastpathresistanceapp/LeastPathResistanceTest.java
@@ -27,6 +27,14 @@ public class LeastPathResistanceTest {
     }
 
     @Test
+    public void test2(){
+        LeastPathResistance leastPathResistance = new LeastPathResistance();
+        String data="3 4 1 2 8 6 6 1 8 2 7 4 5 9 3 9 9 5 8 4 1 3 2 6 3 7 2 1 2 3";
+        String result = leastPathResistance.printleastResistancePath(data, 5, 6);
+        assertEquals(result,"YES  11  1 2 1 5 4 5");
+    }
+
+    @Test
     public void test3(){
         LeastPathResistance leastPathResistance = new LeastPathResistance();
         String data="19 10 19 10 19 21 23 20 19 12 20 12 20 11 10";
