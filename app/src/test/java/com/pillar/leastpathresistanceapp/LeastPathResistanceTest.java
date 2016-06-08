86453edf78ab1f75d9e6a8eafcb94f0f54ac7b33
rkeshav1987 <rkeshav1987>
2016-06-07 21:48:41 -0700

    refactoring code: LeastPathResistanceTest.java

--- app/src/test/java/com/pillar/leastpathresistanceapp/LeastPathResistanceTest.java
+++ app/src/test/java/com/pillar/leastpathresistanceapp/LeastPathResistanceTest.java
@@ -9,36 +9,35 @@ import static org.junit.Assert.*;
  */
 public class LeastPathResistanceTest {
 
+    LeastPathResistance leastPathResistance = new LeastPathResistance();
+    String data="";
+    String result="";
+
     @Test
     public void testInvalidData() {
-
-        LeastPathResistance leastPathResistance = new LeastPathResistance();
-        String data = "1 2 3 4 5 6";
-        String result = leastPathResistance.printleastResistancePath(data, 5, 6);
+        data = "1 2 3 4 5 6";
+        result = leastPathResistance.printleastResistancePath(data, 5, 6);
         assertEquals("Invalid Data", result);
     }
 
     @Test
     public void test1(){
-        LeastPathResistance leastPathResistance = new LeastPathResistance();
-        String data="3 4 1 2 8 6 6 1 8 2 7 4 5 9 3 9 9 5 8 4 1 3 2 6 3 7 2 8 6 4";
-        String result = leastPathResistance.printleastResistancePath(data, 5, 6);
+        data="3 4 1 2 8 6 6 1 8 2 7 4 5 9 3 9 9 5 8 4 1 3 2 6 3 7 2 8 6 4";
+        result = leastPathResistance.printleastResistancePath(data, 5, 6);
         assertEquals("YES  16  1 2 3 4 4 5",result);
     }
 
     @Test
     public void test2(){
-        LeastPathResistance leastPathResistance = new LeastPathResistance();
-        String data="3 4 1 2 8 6 6 1 8 2 7 4 5 9 3 9 9 5 8 4 1 3 2 6 3 7 2 1 2 3";
-        String result = leastPathResistance.printleastResistancePath(data, 5, 6);
+        data="3 4 1 2 8 6 6 1 8 2 7 4 5 9 3 9 9 5 8 4 1 3 2 6 3 7 2 1 2 3";
+        result = leastPathResistance.printleastResistancePath(data, 5, 6);
         assertEquals(result,"YES  11  1 2 1 5 4 5");
     }
 
     @Test
     public void test3(){
-        LeastPathResistance leastPathResistance = new LeastPathResistance();
-        String data="19 10 19 10 19 21 23 20 19 12 20 12 20 11 10";
-        String result = leastPathResistance.printleastResistancePath(data, 3, 5);
+        data="19 10 19 10 19 21 23 20 19 12 20 12 20 11 10";
+        result = leastPathResistance.printleastResistancePath(data, 3, 5);
         assertEquals("NO  48  1 1 1",result);
     }
 }
